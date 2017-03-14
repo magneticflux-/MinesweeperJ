2e93e33dbb266e9c60116ce076a57e71a624ba05
Mitchell Skaggs <skaggsm333@gmail.com>
2017-03-13 19:40:08 -0500

    Added initial Game class

---
+++ src/main/java/org/skaggsm/minesweeperj/board/Game.java
@@ -0,0 +1,24 @@
+package org.skaggsm.minesweeperj.board;
+
+/**
+ * This class defines a game board and a method to make a move on the board
+ *
+ * @param <M> The type of the object that encapsulates a move
+ * @param <S> The type of the object that defines the publicly visible game state
+ * @author Mitchell Skaggs
+ */
+public interface Game<M, S> {
+    /**
+     * Makes a move on the game board using the provided move data
+     *
+     * @param move the move data to use
+     */
+    void makeMove(M move);
+
+    /**
+     * Gets the current game state.
+     *
+     * @return The current game state. Recommended to be immutable.
+     */
+    S getGameState();
+}
