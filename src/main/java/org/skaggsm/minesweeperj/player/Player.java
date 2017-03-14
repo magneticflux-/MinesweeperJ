f23283691a9b7127cfe70e3a538d94c41e869970
Mitchell Skaggs <skaggsm333@gmail.com>
2017-03-14 09:00:54 -0500

    Added Player interface

---
+++ src/main/java/org/skaggsm/minesweeperj/player/Player.java
@@ -0,0 +1,10 @@
+package org.skaggsm.minesweeperj.player;
+
+/**
+ * This class defines the identity of player of a game.
+ *
+ * @author Mitchell Skaggs
+ */
+public interface Player {
+    String getName();
+}
