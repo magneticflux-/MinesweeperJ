5bcb74975474c722917be73698d08f9900c06490
Mitchell Skaggs <skaggsm333@gmail.com>
2017-03-14 09:01:21 -0500

    Added Move interface

---
+++ src/main/java/org/skaggsm/minesweeperj/board/Move.java
@@ -0,0 +1,10 @@
+package org.skaggsm.minesweeperj.board;
+
+import org.skaggsm.minesweeperj.player.Player;
+
+/**
+ * @author Mitchell Skaggs
+ */
+public interface Move {
+    Player getPlayer();
+}
