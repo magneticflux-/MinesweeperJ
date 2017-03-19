eb2403e5cb45ae4a446ede77dec010e17dd4f7c9
Mitchell Skaggs <skaggsm333@gmail.com>
2017-03-19 11:55:26 -0500

    Created builder with Javadocs for the MinesweeperMove class and annotated both

--- src/main/java/org/skaggsm/minesweeperj/board/MinesweeperMove.java
+++ src/main/java/org/skaggsm/minesweeperj/board/MinesweeperMove.java
@@ -2,6 +2,9 @@ package org.skaggsm.minesweeperj.board;
 
 import org.skaggsm.minesweeperj.entities.Player;
 
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
 /**
  * This class represents an immutable move in a minesweeper game.
  *
@@ -13,7 +16,7 @@ public class MinesweeperMove implements Move {
     private final int column;
     private final MoveType moveType;
 
-    public MinesweeperMove(Player<MinesweeperMove, ? extends View> player, int row, int column, MoveType moveType) {
+    private MinesweeperMove(@Nonnull Player<MinesweeperMove, ? extends View> player, @Nonnegative int row, @Nonnegative int column, @Nonnull MoveType moveType) {
         this.player = player;
         this.row = row;
         this.column = column;
@@ -39,5 +42,110 @@ public class MinesweeperMove implements Move {
 
     public enum MoveType {
         TEST, FLAG
+    }
+
+    /**
+     * This class is a builder for {@link MinesweeperMove}.
+     *
+     * @author Mitchell Skaggs
+     */
+    public static class Builder {
+        private Player<MinesweeperMove, ? extends View> player;
+        private int row;
+        private int column;
+        private MoveType moveType;
+
+        /**
+         * Constructs a new instance with default values.
+         */
+        public Builder() {
+            this.player = null;
+            this.row = -1;
+            this.column = -1;
+            this.moveType = null;
+        }
+
+        /**
+         * Sets the player that made the move.
+         *
+         * @param player The player
+         * @return The builder
+         */
+        public Builder setPlayer(@Nonnull Player<MinesweeperMove, ? extends View> player) {
+            this.player = player;
+            return this;
+        }
+
+
+        /**
+         * Sets the row to which this move will refer.
+         *
+         * @param row The row to set
+         * @return The builder
+         */
+        public Builder setRow(@Nonnegative int row) {
+            this.row = row;
+            return this;
+        }
+
+        /**
+         * Sets the column to which this move will refer.
+         *
+         * @param column The column to set
+         * @return The builder
+         */
+        public Builder setColumn(@Nonnegative int column) {
+            this.column = column;
+            return this;
+        }
+
+        /**
+         * Convenience method. Sets the row and column to which this move will refer.
+         *
+         * @param row    The row to set
+         * @param column The column to set
+         * @return The builder
+         */
+        public Builder setCoordinates(@Nonnegative int row, @Nonnegative int column) {
+            this.setRow(row);
+            this.setColumn(column);
+            return this;
+        }
+
+        /**
+         * Sets the move type of this move.
+         *
+         * @param moveType The type of move
+         * @return The builder
+         */
+        public Builder setMoveType(@Nonnull MoveType moveType) {
+            this.moveType = moveType;
+            return this;
+        }
+
+        /**
+         * Builds a {@link MinesweeperMove}.
+         *
+         * @return a new MinesweeperMove
+         * @throws NullPointerException if {@link Builder#setPlayer(Player)} or {@link Builder#setMoveType(MoveType)} was not called
+         */
+        public MinesweeperMove build() {
+            checkArguments();
+            return new MinesweeperMove(player, row, column, moveType);
+        }
+
+        /**
+         * Throws {@link IllegalStateException} if the builder has not been fully constructed before the {@link Builder#build()} method is called.
+         */
+        private void checkArguments() {
+            if (this.player == null)
+                throw new IllegalStateException("\"setPlayer\" must be called before building!");
+            if (row < 0)
+                throw new IllegalStateException("\"setRow\" must be called before building!");
+            if (column < 0)
+                throw new IllegalStateException("\"setColumn\" must be called before building!");
+            if (moveType == null)
+                throw new IllegalStateException("\"setMoveType\" must be called before building!");
+        }
     }
 }
