765027f0cca3317cd5aaa6ce74cea639c34b5b5c
Mitchell Skaggs <skaggsm333@gmail.com>
2017-03-20 18:11:16 -0500

    Manually initialized @Mock field in @BeforeClass method because of @DataPoint annotation

--- src/test/java/org/skaggsm/minesweeperj/board/GameTest.java
+++ src/test/java/org/skaggsm/minesweeperj/board/GameTest.java
@@ -1,5 +1,6 @@
 package org.skaggsm.minesweeperj.board;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.experimental.categories.Category;
 import org.junit.experimental.theories.DataPoint;
@@ -8,15 +9,15 @@ import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 import org.mockito.Answers;
 import org.mockito.Mock;
-import org.mockito.internal.junit.JUnitRule;
-import org.mockito.internal.util.ConsoleMockitoLogger;
+import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.quality.Strictness;
 import org.skaggsm.categories.UnitTests;
 import org.skaggsm.minesweeperj.entities.Player;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
 
 /**
  * This class test the {@link Game} interface's default method {@link Game#addPlayer(Player)}.
@@ -29,11 +30,20 @@ public class GameTest {
     @Mock
     @DataPoint
     public static Player<Move, View> SIMPLE_PLAYER;
+
     @Rule
-    public final MockitoRule mockitoRule = new JUnitRule(new ConsoleMockitoLogger(), Strictness.STRICT_STUBS);
+    public final MockitoRule mockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
 
     @Mock(answer = Answers.CALLS_REAL_METHODS)
     private Game<Move, View> game;
+
+    @SuppressWarnings("unchecked")
+    @BeforeClass
+    public static void setup() {
+        SIMPLE_PLAYER = mock(Player.class); //Needed because of @DataPoint usage
+        when(SIMPLE_PLAYER.getName()).thenReturn("Simple Player");
+        when(SIMPLE_PLAYER.getIdentification()).thenReturn(new UUID(0, 0));
+    }
 
     @Theory
     public void Given_AnyPlayer_When_AddPlayerCalled_Then_AddParticipantCalledAddViewerCalled(Player<Move, View> anyPlayer) throws Exception {
