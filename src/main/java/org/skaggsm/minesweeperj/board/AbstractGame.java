43e9421d4904430dd27e6757efe8f31346c919c0
Mitchell Skaggs <skaggsm333@gmail.com>
2017-03-22 15:35:38 -0500

    Added AbstractGame

--- src/main/java/org/skaggsm/minesweeperj/board/AbstractGame.java
+++ src/main/java/org/skaggsm/minesweeperj/board/AbstractGame.java
@@ -1,19 +1,27 @@
 package org.skaggsm.minesweeperj.board;
 
-import org.skaggsm.minesweeperj.entities.Player;
+import org.skaggsm.minesweeperj.entities.Viewer;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * This class defines a game board and a method to make a move on the board.
+ * This class defines a game.
  *
  * @param <M> The type of the object that encapsulates a move
  * @param <V> The type of the object that defines a view of the game state
  * @author Mitchell Skaggs
  */
-public abstract class AbstractGame<M extends Move, V extends View> implements Viewable<V>, Participable<M, V>, Playable<M, V> {
-    public void addPlayer(@Nonnull Player<M, V> player) {
-        addViewer(player);
-        addParticipant(player);
+public abstract class AbstractGame<M extends Move, V extends View> implements Game<M, V> {
+    protected List<Viewer<V>> viewers;
+
+    public AbstractGame() {
+        viewers = new ArrayList<>();
+    }
+
+    @Override
+    public void addViewer(@Nonnull Viewer<V> viewer) {
+        viewers.add(viewer);
     }
 }
---
+++ src/main/java/org/skaggsm/minesweeperj/board/Game.java
@@ -0,0 +1,27 @@
+package org.skaggsm.minesweeperj.board;
+
+import org.skaggsm.minesweeperj.entities.Player;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author Mitchell Skaggs
+ */
+public interface Game<M extends Move, V extends View> extends Viewable<V>, Participable<M, V>, Playable<M, V> {
+    /**
+     * Adds a player to this game.
+     *
+     * @param player the player to add
+     */
+    default void addPlayer(@Nonnull Player<M, V> player) {
+        addViewer(player);
+        addParticipant(player);
+    }
+
+    /**
+     * Submits a move from the included player's perspective.
+     *
+     * @param move the move to make
+     */
+    void makeMove(M move);
+}
