d2db66ef1a42b5d3ca66eadbe89cac85ea07a185
Mitchell Skaggs <skaggsm333@gmail.com>
2017-03-24 07:43:11 -0500

    Added remove method to Participable

--- src/main/java/org/skaggsm/minesweeperj/board/Participable.java
+++ src/main/java/org/skaggsm/minesweeperj/board/Participable.java
@@ -11,7 +11,14 @@ public interface Participable<M extends Move, V extends View> {
     /**
      * Adds a participant to this Participable.
      *
-     * @param participant The participant to add
+     * @param participant the participant to add
      */
     void addParticipant(@Nonnull Participant<M, V> participant);
+
+    /**
+     * Removes a participant from this Participable.
+     *
+     * @param participant the participant to remove
+     */
+    void removeParticipant(@Nonnull Participant<M, V> participant);
 }
