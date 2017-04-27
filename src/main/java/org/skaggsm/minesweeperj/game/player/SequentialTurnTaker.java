4a4276a6fe6d6b16198734c0b13e606564eaa899
Mitchell Skaggs <skaggsm333@gmail.com>
2017-04-27 10:19:29 -0500

    Added SequentialTurnTaker and tests

---
+++ src/main/java/org/skaggsm/minesweeperj/game/player/SequentialTurnTaker.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2017 Mitchell Skaggs
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.skaggsm.minesweeperj.game.player;
+
+import com.google.common.collect.Iterators;
+import org.skaggsm.minesweeperj.game.MinesweeperGame;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Mitchell Skaggs
+ */
+public class SequentialTurnTaker implements TurnTaker {
+    private final MinesweeperGame minesweeperGame;
+    private final Iterator<MinesweeperPlayer> players;
+
+    public SequentialTurnTaker(MinesweeperGame minesweeperGame, List<MinesweeperPlayer> players) {
+        this.minesweeperGame = minesweeperGame;
+        this.players = Iterators.cycle(players);
+    }
+
+    public SequentialTurnTaker(MinesweeperGame minesweeperGame, MinesweeperPlayer... players) {
+        this(minesweeperGame, Arrays.asList(players));
+    }
+
+    @Override
+    public void takeNextTurn() {
+        minesweeperGame.makeMove(players.next().getMove(minesweeperGame));
+    }
+}
